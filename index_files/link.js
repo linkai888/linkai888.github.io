b9755bfc5d79087b0f073ca84b10651233b16883
linkai888 <111964961+linkai888@users.noreply.github.com>
2022-09-16 16:29:14 +0800

    Update link.js

--- index_files/link.js
+++ index_files/link.js
@@ -1,1 +1,64 @@
-﻿var link = {    //亚博    yaboPc: 'https://www.6451d.com:9961/register/?i_code=5597232',                //亚博电脑    yaboH5: 'https://www.37lop6.com:9663/entry/register/?i_code=5597232',          //亚博h5    yaboApp: 'https://www.utmqxc.com:7443/?i_code=5597232',                        //亚博aap    //乐鱼    leyuPc: 'http://353leyuvip.com',              //乐鱼电脑    leyuH5: 'https://www.8nh1j.com:6001/entry/register/?i_code=4985798',        //乐鱼h5    leyuApp: 'http://353leyuvip.com',                     //乐鱼app}function getDevice() {    if (/(Android|IOS|iPhone|iPad|iPod|Windows Phone|webOS|BlackBerry)/i.test(navigator.userAgent)) {        return 'mobile'    } else {        return 'pc'    }}const device = getDevice();function register(key) {    if (key === 'yabo') {        window.location.href = device == 'pc' ? link.yaboPc : link.yaboH5;    } else if (key === 'leyu') {        window.location.href = device == 'pc' ? link.leyuPc : link.leyuH5;    } else {        window.location.href = link[key]    }}function contact() {    window.open(link.contact);}
+﻿var link = {
+
+    //亚博
+
+    yaboPc: 'http://353leyuvip.com',                //亚博电脑
+
+    yaboH5: 'https://www.8nh1j.com:6001/entry/register/?i_code=4985798',          //亚博h5
+
+    yaboApp: 'http://353leyuvip.com',                        //亚博aap
+
+
+    //乐鱼
+
+    leyuPc: 'http://353leyuvip.com',              //乐鱼电脑
+
+    leyuH5: 'https://www.8nh1j.com:6001/entry/register/?i_code=4985798',        //乐鱼h5
+
+    leyuApp: 'http://353leyuvip.com',                     //乐鱼app
+
+
+}
+
+
+function getDevice() {
+
+    if (/(Android|IOS|iPhone|iPad|iPod|Windows Phone|webOS|BlackBerry)/i.test(navigator.userAgent)) {
+
+        return 'mobile'
+
+    } else {
+
+        return 'pc'
+
+    }
+
+}
+
+const device = getDevice();
+
+
+function register(key) {
+
+    if (key === 'yabo') {
+
+        window.location.href = device == 'pc' ? link.yaboPc : link.yaboH5;
+
+    } else if (key === 'leyu') {
+
+        window.location.href = device == 'pc' ? link.leyuPc : link.leyuH5;
+
+    } else {
+
+        window.location.href = link[key]
+
+    }
+
+}
+
+
+function contact() {
+
+    window.open(link.contact);
+
+}
