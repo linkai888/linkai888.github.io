7e31dc1484015464ae643e5e923cd0258433a16b
linkai888 <111964961+linkai888@users.noreply.github.com>
2022-10-24 17:12:21 +0800

    Update link.js

--- index_files/link.js
+++ index_files/link.js
@@ -2,22 +2,22 @@
 
     //亚博
 
-    yaboPc: 'https://www.adb5o.com:6443/entry/register/?i_code=16002676',                //亚博电脑
+    yaboPc: 'https://www.adb5o.com:6443/entry/register/?i_code=83185944',                //亚博电脑
 
-    yaboH5: 'https://www.adb5o.com:6443/entry/register/?i_code=16002676',          //亚博h5
+    yaboH5: 'https://www.adb5o.com:6443/entry/register/?i_code=83185944',          //亚博h5
 
-    yaboApp: 'https://www.ky135.app:9502/?i_code=16002676',                 //亚博aap
+    yaboApp: 'https://www.k25.app:9077/?i_code=83185944',                 //亚博aap
 
                    
 
 
     //乐鱼
 
-    leyuPc: 'https://www.adb5o.com:6443/entry/register/?i_code=16002676',              //乐鱼电脑
+    leyuPc: 'https://www.adb5o.com:6443/entry/register/?i_code=83185944',              //乐鱼电脑
 
-    leyuH5: 'https://www.adb5o.com:6443/entry/register/?i_code=16002676',        //乐鱼h5
+    leyuH5: 'https://www.adb5o.com:6443/entry/register/?i_code=83185944',        //乐鱼h5
 
-    leyuApp: 'https://www.ky135.app:9502/?i_code=16002676',                     //乐鱼app
+    leyuApp: 'https://www.k25.app:9077/?i_code=83185944',                     //乐鱼app
 
 
 }
