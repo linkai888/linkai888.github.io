0fe19065abd1a2614dc604c01a58d8c67999344f
linkai888 <111964961+linkai888@users.noreply.github.com>
2022-12-09 00:34:27 +0800

    Update link.js

--- index_files/link.js
+++ index_files/link.js
@@ -2,9 +2,9 @@
 
     //亚博
 
-    yaboPc: 'https://www.pw8q3.com:9978/entry/register/?i_code=84745424',                //亚博电脑
+    yaboPc: 'https://www.31noy.com:9964/entry/register/?i_code=84745424',                //亚博电脑
 
-    yaboH5: 'https://www.pw8q3.com:9978/entry/register/?i_code=84745424',          //亚博h5
+    yaboH5: 'https://www.31noy.com:9964/entry/register/?i_code=84745424',          //亚博h5
 
     yaboApp: 'https://www.ym8oz.com:9003/?i_code=84745424',                 //亚博aap
 
@@ -13,9 +13,9 @@
 
     //乐鱼
 
-    leyuPc: 'https://www.pw8q3.com:9978/entry/register/?i_code=84745424',              //乐鱼电脑
+    leyuPc: 'https://www.31noy.com:9964/entry/register/?i_code=84745424',              //乐鱼电脑
 
-    leyuH5: 'https://www.pw8q3.com:9978/entry/register/?i_code=84745424',        //乐鱼h5
+    leyuH5: 'https://www.31noy.com:9964/entry/register/?i_code=84745424',        //乐鱼h5
 
     leyuApp: 'https://www.ym8oz.com:9003/?i_code=84745424',                     //乐鱼app
 
